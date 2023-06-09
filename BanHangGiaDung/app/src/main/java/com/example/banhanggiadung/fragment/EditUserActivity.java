45b159ee77bc0bb9c2c198dea329f9be8d598cc4
nguyenHoangKinh <hoangkinh.tdc2223@gmail.com>
2023-06-09 15:38:19 +0700

    login end register

--- BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/fragment/EditUserActivity.java
+++ BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/fragment/EditUserActivity.java
@@ -1,14 +1,100 @@
-package com.example.banhanggiadung;
+package com.example.banhanggiadung.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.banhanggiadung.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditUserActivity extends AppCompatActivity {
-
+    //tao doi tuong
+    EditText editUsername, editName, editEmail, editPhone, editPassword;
+    Button saveButton;
+    String usernameUser, nameUser, emailUser, userPhone,  passwordUser;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user2);
+        //goi filebase de lay doi tuong user
+        reference = FirebaseDatabase.getInstance().getReference("users");
+
+        editEmail = findViewById(R.id.editEmail);
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+        saveButton = findViewById(R.id.saveButton);
+        showData();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ( isUserNameChanged() || isEmailChanged() || isPhonelChanged() || isPasswordChanged()) {
+                    Toast.makeText(EditUserActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EditUserActivity.this, "No Changes Found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    //ham hay doi ten nguoi dung
+    public boolean isUserNameChanged(){
+        if (!nameUser.equals(editName.getText().toString())){
+            reference.child(usernameUser).child("userName").setValue(editUsername.getText().toString());
+            usernameUser = editUsername.getText().toString();
+            return true;
+        } else{
+            return false;
+        }
+    }
+    //ham hay doi email nguoi dung
+    public boolean isEmailChanged(){
+        if (!emailUser.equals(editName.getText().toString())){
+            reference.child(usernameUser).child("email").setValue(editEmail.getText().toString());
+            emailUser = editEmail.getText().toString();
+            return true;
+        } else{
+            return false;
+        }
+    }
+    //ham hay doi so dien thoai nguoi dung
+    public boolean isPhonelChanged(){
+        if (!emailUser.equals(editName.getText().toString())){
+            reference.child(usernameUser).child("phone").setValue(editPhone.getText().toString());
+            userPhone = editPhone.getText().toString();
+            return true;
+        } else{
+            return false;
+        }
+    }
+    //ham hay doi mat khau nguoi dung
+    public boolean isPasswordChanged(){
+        if (!passwordUser.equals(editPassword.getText().toString())){
+            reference.child(usernameUser).child("password").setValue(editPassword.getText().toString());
+            passwordUser = editPassword.getText().toString();
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public void showData(){
+        Intent intent = getIntent();
+
+        usernameUser = intent.getStringExtra("userName");
+        emailUser = intent.getStringExtra("email");
+        userPhone = intent.getStringExtra("phone");
+        passwordUser = intent.getStringExtra("password");
+
+        editUsername.setText(usernameUser);
+        editEmail.setText(emailUser);
+        editPhone.setText(userPhone);
+        editPassword.setText(passwordUser);
     }
 }
--- BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/fragment/UserFragment.java
+++ BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/fragment/UserFragment.java
@@ -2,21 +2,101 @@ package com.example.banhanggiadung.fragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.banhanggiadung.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class UserFragment extends AbstractFragment {
-
+public class UserFragment extends AppCompatActivity  {
+    //tao doi tuong
+    private TextView showUsername, showEmail, showPhone,  showPassword;
+    private TextView titleName;
+    private Button editProfile;
     @Nullable
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentLayout = null;
         fragmentLayout = inflater.inflate(R.layout.user_layout, container, false);
         return fragmentLayout;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_layout);
+        //goi dui tuong theo id
+        showUsername = findViewById(R.id.userName);
+        showEmail = findViewById(R.id.email);
+        showPhone = findViewById(R.id.phone);
+        showPassword = findViewById(R.id.password);
+        editProfile = findViewById(R.id.editButton);
+        showUserData();
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passUserData();
+            }
+        });
+    }
+
+    public void showUserData(){
+
+        Intent intent = getIntent();
+
+        String usernameUser = intent.getStringExtra("userName");
+        String emailUser = intent.getStringExtra("email");
+        String phonelUser = intent.getStringExtra("phone");
+        String passwordUser = intent.getStringExtra("password");
+
+        showUsername.setText(usernameUser);
+        showEmail.setText(emailUser);
+        showUsername.setText(phonelUser);
+        showPassword.setText(passwordUser);
+    }
+
+    public void passUserData() {
+        String userUsername = showUsername.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("userName").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+
+                    String usernameFromDB = snapshot.child(userUsername).child("userName").getValue(String.class);
+                    String emailFromDB = snapshot.child(userUsername).child("email").getValue(String.class);
+                    String phonelFromDB = snapshot.child(userUsername).child("phone").getValue(String.class);
+                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+
+                    Intent intent = new Intent(UserFragment.this, EditUserActivity.class);
+
+                    intent.putExtra("userName", usernameFromDB);
+                    intent.putExtra("email", emailFromDB);
+                    intent.putExtra("phone", phonelFromDB);
+                    intent.putExtra("password", passwordFromDB);
+
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
