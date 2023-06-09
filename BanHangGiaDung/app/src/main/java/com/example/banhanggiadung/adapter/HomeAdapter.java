421d6347ed76fab44c4d28733fa498a89c9c4039
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-06-09 18:03:59 +0700

    up

--- BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/adapter/HomeAdapter.java
+++ BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/adapter/HomeAdapter.java
@@ -1,2 +1,59 @@
-package com.example.banhanggiadung.adapter;public class HomeAdapter {
+package com.example.banhanggiadung.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.banhanggiadung.R;
+import com.example.banhanggiadung.model.Product;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class HomeAdapter extends FirebaseRecyclerAdapter<Product, HomeAdapter.myViewHolder> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public HomeAdapter(@NonNull FirebaseRecyclerOptions<Product> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull Product model) {
+        holder.name.setText(model.getName());
+//        holder.price.setText(model.getPrice());
+        Glide.with(holder.img.getContext())
+                .load(model.getImage())
+                .placeholder(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark)
+                .circleCrop()
+                .error(com.google.firebase.database.R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.img);
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_products, parent, false);
+        return new myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView name, price;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = (ImageView) itemView.findViewById(R.id.img_product);
+            name = (TextView) itemView.findViewById(R.id.name_product);
+            price = (TextView) itemView.findViewById(R.id.price_product);
+        }
+    }
 }
--- BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/adapter/ProductAdapter.java
+++ BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/adapter/ProductAdapter.java
@@ -1,5 +1,6 @@
 package com.example.banhanggiadung.adapter;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,43 +8,61 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.banhanggiadung.R;
 import com.example.banhanggiadung.model.Product;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
-    private Activity activity;
-    private int layoutID;
-    private ArrayList<Product> productList;
+//    private Activity activity;
+//    private int layoutID;
+    private List<Product> productList;
 
-    public ProductAdapter(Activity activity, int layoutID, ArrayList<Product> productList) {
-        this.activity = activity;
-        this.layoutID = layoutID;
+    public ProductAdapter(List<Product> productList) {
+//        this.activity = activity;
+//        this.layoutID = layoutID;
         this.productList = productList;
     }
+
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        LayoutInflater inflater = activity.getLayoutInflater();
-        CardView view = (CardView) inflater.inflate(viewType, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_products, viewGroup, false);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        int img = productList.get(position).getHinhSanPham();
-        String name = String.valueOf(productList.get(position).getTenSanPham());
-        String price = String.valueOf(productList.get(position).getGiaSanPham());
-//        holder.imgProduct.setImageResource(img);
-        holder.imgProduct.setImageDrawable(activity.getResources().getDrawable(img, activity.getTheme()));
-        holder.name_product.setText(name);
-        holder.price_product.setText(price);
+        Product data = productList.get(position);
+//        holder.imgProduct.setImageDrawable(data.getHinhSanPham());
+//        int img = productList.get(position).getHinhSanPham();
+        Glide.with(holder.itemView.getContext())
+                .asBitmap()
+//                .load(img)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+                        // Chuyển đổi Bitmap thành kiểu int
+                        int imageInt = resource.getPixel(0, 0);
+                        holder.imgProduct.setImageResource((int) imageInt);
+                    }
+                });
+//        holder.name_product.setText(data.getTenSanPham());
+//        holder.price_product.setText(data.getGiaSanPham());
+//        String name = String.valueOf(productList.get(position).getTenSanPham());
+//        String price = String.valueOf(productList.get(position).getGiaSanPham());
+////        holder.imgProduct.setImageResource(img);
+//        holder.imgProduct.setImageDrawable(activity.getResources().getDrawable(img, activity.getTheme()));
+//        holder.name_product.setText(name);
+//        holder.price_product.setText(price);
     }
 
     @Override
@@ -59,16 +78,21 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHo
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             imgProduct = itemView.findViewById(R.id.img_product);
-            name_product = itemView.findViewById(R.id.tv_name_product);
-            price_product = itemView.findViewById(R.id.tv_price_product);
+            name_product = itemView.findViewById(R.id.name_product);
+            price_product = itemView.findViewById(R.id.price_product);
 
 
         }
-    }
 
-    @Override
-    public int getItemViewType(int position) {
-        return layoutID;
 
     }
+
+    public void setProductList(ArrayList<Product> productList) {
+        this.productList = productList;
+    }
+//    @Override
+//    public int getItemViewType(int position) {
+//        return layoutID;
+//
+//    }
 }
