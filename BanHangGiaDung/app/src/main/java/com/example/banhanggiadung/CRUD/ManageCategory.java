3695bee26a56e41772a4fea22339af214af98db4
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-06-09 22:26:04 +0700

    commit manage activity and layout

--- BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/CRUD/ManageCategory.java
+++ BanHangGiaDung/app/src/main/java/com/example/banhanggiadung/CRUD/ManageCategory.java
@@ -1,8 +1,10 @@
-package com.example.banhanggiadung;
+package com.example.banhanggiadung.CRUD;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.banhanggiadung.R;
 
 public class ManageCategory extends AppCompatActivity {
 
